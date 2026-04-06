ce65b77ca29236d563d623041d39ff141624d8ec
Ruben Alberto Rodriguez Pachano <92474354+rubenrodriguezchiv@users.noreply.github.com>
2026-04-06 08:47:29 -0400

    Add first files

---
+++ assets/js/sidebar.js
@@ -0,0 +1,61 @@
+const menusItemsDropDown = document.querySelectorAll('.menu-item-dropdown');
+const menuitemStatic = document.querySelectorAll('.menu-item-static');
+const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menu-btn');
+const sidebarBtn = document.getElementById('sidebar-btn');
+
+sidebarBtn.addEventListener('click', ()=>{
+    document.body.classList.toggle('sidebar-hidden');
+});
+
+menuBtn.addEventListener('click', ()=>{
+    sidebar.classList.toggle('minimize');
+});
+
+menusItemsDropDown.forEach((menuItem)=>{
+    menuItem.addEventListener('click', ()=>{
+        const subMenu = menuItem.querySelector('.submenu');
+        const isActive = menuItem.classList.toggle('sub-menu-toggle');
+        if(subMenu){
+            if(isActive){
+                subMenu.style.height = `${subMenu.scrollHeight +6}px`;
+                subMenu.style.padding = '0.2rem 0';
+            }else{
+                subMenu.style.height = '0';
+                subMenu.style.padding = '0';
+            }
+        }
+        menusItemsDropDown.forEach((item)=>{
+            if(item !==menuItem){
+                const otherSubmenu = item.querySelector('.submenu');
+                if (otherSubmenu){
+                    item.classList.remove('sub-menu-toggle');
+                    otherSubmenu.style.height = '0';
+                    otherSubmenu.style.padding = '0';
+                }
+            }
+        });
+    });
+});
+
+menuitemStatic.forEach((menuItem)=>{
+    menuItem.addEventListener('mouseenter', ()=>{
+
+        if(!sidebar.classList.contains('minimize')) return;
+
+        menusItemsDropDown.forEach((item)=>{
+                const otherSubmenu = item.querySelector('.submenu');
+                if (otherSubmenu){
+                    item.classList.remove('sub-menu-toggle');
+                    otherSubmenu.style.height = '0';
+                    otherSubmenu.style.padding = '0';
+                }
+        });
+    });
+});
+
+function checkWindowsSize(){
+    sidebar.classList.remove('minimize');
+}
+checkWindowsSize();
+window.addEventListener('resize',checkWindowsSize);
